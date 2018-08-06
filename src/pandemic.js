adfaf9e1f025667f994eb9db4bf6ea76e1d42a3e
Devin Mounts and nicholas brown <devin@>
2018-08-06 11:52:02 -0700

    loop object function

--- src/pandemic.js
+++ src/pandemic.js
@@ -2,6 +2,54 @@ export class Pandemic {
   constructor (name) {
     this.name = name;
     this.outbreakCounter = 1;
-    this.cities = { city1:10,city2:10,city3:10,city4:10,city5:10}
+    this.cities = { Portland :10, Beaverton:10, Bend:10, Corvallis:10, Salem:10}
   }
+
+  setOutbreak() {
+    setInterval(() => {
+      this.outbreakCounter++;
+    }, 30000)
+  }
+
+  // setOutbreak() {
+  //   setInterval(() => {
+  //     this.cities.Portland -=3;
+  //   })
+  // }
+
+  cureOutbreak() {
+    this.outbreakCounter--
+  }
+
+  didTheOutbreakTakeOver() {
+    if (this.outbreakCounter <  5) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  getCitiesStatus() {
+    let i = 0;
+    for (; i < 3 ;) {
+
+
+    }
+    for (let key in this.cities) {
+      if (this.cities.hasOwnProperty(key)) {
+        console.log(this.cities[key])
+        if (this.cities[key] < 4) {
+          console.log("reached")
+            this.cities[key]-= i;
+            i+=1;
+            console.log( key+"-->"+this.cities[key]);
+          }
+        }
+        console.log( key+"-->"+this.cities[key]);
+      }
+    }
+  }
+  }
+
+
 }
