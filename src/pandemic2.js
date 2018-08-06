ae9b03f9021a1086d15c60ac2b2dd58f8e6953cb
Devin Mounts and nicholas brown <devin@>
2018-08-06 16:49:48 -0700

    end of monday1.

--- src/pandemic2.js
+++ src/pandemic2.js
@@ -2,8 +2,9 @@ export let Pandemic = {
   outbreakCounter : 0,
   setOutbreak: function() {
     const outbreakInterval = setInterval(() => {
-      this.outbreakCounter++
-    })
+      this.outbreakCounter++;
+      if (this.did)
+    }
   }
 
 }
